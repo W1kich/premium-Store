623e77dfd42eccbcd8a0cb3e7f13228ea8ffef49
W1kich <denis@example.com>
2026-01-14 19:13:59 +0200

    Add an adaptive feature to the SE phones

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/app/App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
